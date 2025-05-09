b0b4a2ad70dbe5b106817494bddcf9961237e252
Madan Gurung <64871092+madang804@users.noreply.github.com>
2025-05-09 19:34:10 +0100

    Fix linting

--- application.py
+++ application.py
@@ -9,13 +9,17 @@ application = Flask(__name__)
 def home():
     return '''
         <h1>How to make an API call</h1>
-        <h3>Append the following path and your chosen location after the url to get the current weather</h3>
+        <h3>Append the following path and your chosen location after the url to get 
+        the current weather</h3>
         <ul>/api/v1.0/weather?location=<b>&#60;input location&#62;</b></ul>
-        <h3>Append the following path and your chosen location after the url to get the current temperature</h3>
+        <h3>Append the following path and your chosen location after the url to get 
+        the current temperature</h3>
         <ul>/api/v1.0/temperature?location=<b>&#60;input location&#62;</b></ul>
-        <h3>Append the following path and your chosen location after the url to get the current wind speed and direction</h3>
+        <h3>Append the following path and your chosen location after the url to 
+        get the current wind speed and direction</h3>
         <ul>/api/v1.0/wind?location=<b>&#60;input location&#62;</b></ul>
-        <h3>Append the following path and your chosen location after the url to get the current humidity</h3>
+        <h3>Append the following path and your chosen location after the url to 
+        get the current humidity</h3>
         <ul>/api/v1.0/humidity?location=<b>&#60;input location&#62;</b></ul>
     '''
 
@@ -23,7 +27,9 @@ def home():
 @application.route('/api/v1.0/weather')
 def weather():
     location = request.args.get('location')
-    weather_list = ['rain', 'hurricanes', 'cloudy', 'partly cloudy', 'blizzard', 'tornado', 'thunderstorm', 'fog', 'lightning', 'snow', 'wind', 'hail', 'winter storm', 'stormy']
+    weather_list = ['rain', 'hurricanes', 'cloudy', 'partly cloudy', 'blizzard',
+                    'tornado', 'thunderstorm', 'fog', 'lightning', 'snow',
+                    'wind', 'hail', 'winter storm', 'stormy']
     weather = random.choice(weather_list)
     return jsonify({
         'location': location,
@@ -61,5 +67,3 @@ def humidity():
         'location': location,
         'humidity': f'{int(humidity)}%'
     })
-
-

de8b57b5cd09cbdf19b97e37f20cff03a74dd500
madan <madtagu@hotmai.co.uk>
2025-06-01 08:16:29 +0100

    Update value for platform_arn

--- terraform/main.tf
+++ terraform/main.tf
@@ -94,5 +94,5 @@ resource "aws_elastic_beanstalk_environment" "env" {
   application         = aws_elastic_beanstalk_application.app.name
   cname_prefix        = "weather-app"
   version_label       = aws_elastic_beanstalk_application_version.app_version.name
-  platform_arn        = "arn:aws:elasticbeanstalk:${var.region}::platform/Docker AL2023/4.5.2"
+  platform_arn        = "arn:aws:elasticbeanstalk:eu-west-2::platform/Docker running on 64bit Amazon Linux 2023/4.5.2"
   }

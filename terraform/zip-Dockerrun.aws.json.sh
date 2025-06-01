7bc069c92017a3d7b57f5b4ad25e71a333df895d
Madan Gurung <64871092+madang804@users.noreply.github.com>
2025-06-01 21:11:35 +0100

    Create zip-Dockerrun.aws.json.sh

---
+++ terraform/zip-Dockerrun.aws.json.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+trap 'printf "Error: Zipping failed"; exit 1' ERR
+
+printf "Zipping appllication.py, requirements.txt, Procfile..."
+zip application.zip ../application.py ../requirements.txt ../Procfile
+printf "Zipping completed\nFile: application.zip\n"

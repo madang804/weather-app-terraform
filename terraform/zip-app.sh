4111fdba8476aaa8666fa59334d72767e62441cb
Madan Gurung <64871092+madang804@users.noreply.github.com>
2025-06-01 21:09:39 +0100

    Delete terraform/zip-app.sh

--- terraform/zip-app.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-trap 'printf "Error: Zipping failed"; exit 1' ERR
-
-printf "Zipping appllication.py, requirements.txt, Procfile..."
-zip application.zip ../application.py ../requirements.txt ../Procfile
-printf "Zipping completed\nFile: application.zip"
